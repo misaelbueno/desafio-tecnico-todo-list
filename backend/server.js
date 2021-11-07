c0b02ff386280df4ea57d40a26543242a7b4c192
Misael Bueno <misa.hbueno@gmail.com>
2021-11-07 07:43:42 -0300

    arrumando arquivo server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const cors = require('cors');
-const TaskController = require('./controller/TaskController');
 
 const app = express();
 const http = require('http').createServer(app); // cria o servidor para o socket rodar
 const io = require('socket.io')(http, {
   cors: {
     origin: 'http://localhost:3000', // endereço em que o socket vai aceitar requisições
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-  }
-})
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  },
+});
+
+const TaskController = require('./controller/TaskController');
 
 app.use(express.json());
 app.use(cors());
 
 require('./sockets/status')(io);
 require('./sockets/tasks')(io);
-
 
 const PORT = 3001;
 
